1c98239ea1c77bd40a0fef13a53c55c65bc8a3e7
Robert Kirberich <robert@kirberich.de>
2015-06-18 00:10:17 +0100

    Update simple menu sketch, showing button, toggle button, dial and a callback

--- examples/simple_menu/simple_menu.ino
+++ examples/simple_menu/simple_menu.ino
@@ -1,34 +1,50 @@
 #include <Adafruit_GFX.h>    // Core graphics library
-#include <Adafruit_TFTLCD.h> // Hardware-specific library
-#include <TouchScreen.h>
+#include <ILI9341_t3.h>
+#include <UTouch.h>
 #include <gfx_ui.h>
+#include <SPI.h>
 
-TouchScreen ts = TouchScreen(6, A1, A2, 7, 300);
-Adafruit_TFTLCD tft(A3, A2, A1, A0, A4);
-GFX_UI ui(tft, ts, 179, 145, 856, 920);
+GFX_UI ui;
 
-uint16_t text_color = rgb(100, 100, 200);
-uint16_t button_color = rgb(0, 0, 100);
+Color text_color = {200, 200, 200};
+Color button_color = {80, 80, 80};
 
-uint16_t rgb(byte red, byte green, byte blue) {
-  return (((31*(red+4))/255)<<11) | (((63*(green+2))/255)<<5) | ((31*(blue+4))/255);
+void callback1(UIElement& caller) {
+	// If caller is needed, cast it like so:
+	// Button& call_button = static_cast<Button&>(caller);
+
+	Color new_color = {random(255), random(255), random(255)};
+	ui.set_bg_color(new_color);
+	Dial* pdial = static_cast<Dial*>(ui.get_element_by_name("dial"));
+	pdial->set_value(pdial->get_value()+5);
 }
 
-void callback1() {
-}
-Button button1 = Button(40, 40, tft.width()-80, 40, "Button 1", callback1, button_color, text_color);
+Button button1 = Button("button1", 40, 40, 0, 40, "Button 1", callback1, button_color, text_color);
+ToggleButton button2 = ToggleButton("button2", 40, 79, 0, 40, "Button 1", callback1, button_color.encode(), text_color.encode());
+Dial dial = Dial("dial", 0, 150, 20, -10, 100, 1, button_color, text_color);
 
-Label header = Label(0, 0, tft.width(), 20, "Testing", 2, 0, text_color);
+Label header = Label("label", 0, 0, 0, 20, "Testing", 2, 0, text_color.encode());
+unsigned long last_frame = millis();
 
 void setup(void) {
-  Serial.begin(9600);
+	Serial.begin(9600);
+	ui.init(0, 1);
 
-  tft.reset();
-  uint16_t identifier = tft.readID();
-  tft.begin(identifier);
+	// Set values that might depend on the ui rotation after initialising the ui.
+	button1.set_width(ui.width()-80);
+	button2.set_width(ui.width()-80);
+	header.set_width(ui.width());
+	dial.set_x(ui.width()/2);
 
-  ui.add(button1);
-  ui.add(header);
+	// Draw pretty background, before adding (and thereby drawing) elements
+	for (int y=0; y<ui.get_lcd().height(); y++) {
+		ui.get_lcd().drawLine(0, y, ui.get_lcd().width(), y, Color({y-150, y-150, y-150}));
+	}
+
+	ui.add(button1);
+	ui.add(button2);
+	ui.add(header);
+	ui.add(dial);
 }
 
 void loop() {
